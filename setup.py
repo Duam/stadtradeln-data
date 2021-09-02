24cc95754de913879e8ae50d19afd691d2ba23bd
Paul Daum <paul.daum@posteo.de>
2021-09-02 17:29:20 +0200

    Added package meta files.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='stadtradeln_data',
+    version="0.0.1",
+    setup_requires=['setuptools'],
+    use_scm_version=False,
+    install_requires=[
+        'tqdm',
+        'numpy',
+        'pandas'
+    ],
+    python_requires='>=3.6',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    license="MIT",
+    author="Paul Daum",
+    author_email="paul.daum@posteo.de",
+    description="A python package for downloading bicycle traffic count data from the STADTRADELN database",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+)
