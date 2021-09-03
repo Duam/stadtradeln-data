74b0ce6eb6f9efec517a6220e1ee55c60891005e
Paul Daum <paul.daum@posteo.de>
2021-09-03 21:40:26 +0200

    Added default path to stadtradeln_urls.py

--- src/stadtradeln_data/stadtradeln_urls.py
+++ src/stadtradeln_data/stadtradeln_urls.py
@@ -1,3 +1,5 @@
+import pathlib
+
 data_urls = {
     2018: 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/ECF9DF02-37DC-4268-B017-A7C2CF302006/'
           'verkehrsmengen_2018.csv.tar.gz',
@@ -6,3 +8,5 @@ data_urls = {
     2020: 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/ECF9DF02-37DC-4268-B017-A7C2CF302006/'
           'verkehrsmengen_2020.csv.tar.gz',
 }
+
+default_path = pathlib.Path('/tmp/stadtradeln_data/')
