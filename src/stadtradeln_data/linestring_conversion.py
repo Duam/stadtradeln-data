ecc98494f4ae19ac9cc97d4260e3153b02e41fdf
Paul Daum <paul.daum@posteo.de>
2021-09-03 11:28:46 +0200

    Updated docstring.

--- src/stadtradeln_data/linestring_conversion.py
+++ src/stadtradeln_data/linestring_conversion.py
@@ -5,8 +5,7 @@ def get_coordinates_from_linestring(
         linestring: str
 ) -> Tuple[Tuple[float, float], Tuple[float, float]]:
     """Converts a GIS-readable linestring into two coordinate-pairs describing the line's endpoints.
-    :param linestring: A string formatted like "LINESTRING(<latitude1> <longitude2>, <latitude2> <longitude2>)"
-        (including the quotation marks).
+    :param linestring: A string formatted like "LINESTRING(<latitude1> <longitude2>, <latitude2> <longitude2>)".
     :returns: Two coordinate-pairs having latitude and longitude, each.
     """
     endpoints = linestring.replace("LINESTRING(", "").replace(")", "").split(",")
@@ -22,7 +21,6 @@ def get_linestring_from_coordinates(
     """Converts two coordinate-pairs into a GIS-readable linestring-format.
     :param start_coordinate: Latitude and longitude of the line's start point.
     :param end_coordinate: Latitude and longitude of the line's end point.
-    :returns: A string formatted like "LINESTRING(<latitude1> <longitude2>,<latitude2> <longitude2>)"
-        (including the quotation marks).
+    :returns: A string formatted like "LINESTRING(<latitude1> <longitude2>,<latitude2> <longitude2>)".
     """
     return f'LINESTRING({start_coordinate[0]} {start_coordinate[1]},{end_coordinate[0]} {end_coordinate[1]})'
