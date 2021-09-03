868806fed98c1121f25f462127b0b32c2a4b7c9b
Paul Daum <paul.daum@posteo.de>
2021-09-03 17:57:17 +0200

    Moved command-line interface stuff into separate file.

--- apps/stadtradeln_downloader.py
+++ apps/stadtradeln_downloader.py
@@ -1,7 +1,6 @@
 import click
 from stadtradeln_data.status import Status
-from stadtradeln_data.dataset_downloader import download_dataset
-from stadtradeln_data.dataset_extractor import extract_dataset
+from stadtradeln_data.cli_utils import download, extract
 
 
 @click.command()
@@ -17,56 +16,13 @@ def download_and_extract(ctx, year_of_stadtradeln_event, path, overwrite):
     is stored on your local computer in the directory /tmp/stadtradeln_data/.
     """
     year = year_of_stadtradeln_event
-    print(f"Downloading {year} dataset to path \"{path}\"")
-    retry = True
-    verify_ca_certificate = True
-    while retry:
-        download_result = download_dataset(
-            year,
-            destination_path=path,
-            verify_ca_certificate=verify_ca_certificate,
-            overwrite=overwrite
-        )
-        if download_result.status == Status.UNKNOWN_DATASET:
-            print("The dataset you requested does not exist, aborting.")
-            return
-        elif download_result.status == Status.FILE_ALREADY_EXISTS:
-            print(f"File already exists, continuing.")
-            retry = False
-        elif download_result.status == Status.FAILURE:
-            print(f"Download failed, data in \"{download_result.filepath}\" seems corrupted.")
-            return
-        elif download_result.status == Status.SSL_ERROR:
-            print("WARNING: Your CA-certificates could not be verified by the downloader. "
-                  "If you do not trust your network, it is advised that you download the "
-                  "files from the original BmVI website: "
-                  "https://www.mcloud.de/web/guest/suche/-/results/detail/ECF9DF02-37DC-4268-B017-A7C2CF302006 "
-                  f"and manually move them to \"{path}\"")
-            choice = input("Retry without certificate verification (Y/n)? ")
-            verify_ca_certificate = False
-            if choice != "Y" and choice != "y":
-                print("Aborting.")
-                return
-        elif download_result.status == Status.SUCCESS:
-            retry = False
-        else:
-            print("Something unexpectedly went really wrong, sorry. "
-                  "Pleas submit an issue with a short description of the steps "
-                  "you made before encountering this error:")
-            print("https://github.com/Duam/stadtradeln-data/issues/new")
-            print("Aborting.")
-            return
-    print(f"Dataset is located in \"{download_result.filepath}\".")
+    download_result = download(year_of_stadtradeln_event, path, overwrite)
+    if download_result.status != Status.SUCCESS:
+        return
 
-    print(f"Extracting {year} dataset in \"{download_result.filepath}\".")
-    extract_result = extract_dataset(year)
-    if extract_result.status == Status.UNKNOWN_DATASET:
-        print(f"The compressed dataset was not found in path \"{extract_result.filepath}\".")
-    elif extract_result.status == Status.FAILURE:
-        print("Could not extract the dataset (unknown reason).")
-    elif extract_result.status == Status.FILE_ALREADY_EXISTS:
-        print("Extracted file already exists, continuing.")
-    print(f"Raw data is located in \"{extract_result.filepath}\".")
+    extract_result = extract(year, download_result.filepath)
+    if not extract_result.status != Status.SUCCESS:
+        return
 
 
 if __name__ == '__main__':
---
+++ src/stadtradeln_data/cli_utils.py
@@ -0,0 +1,76 @@
+import pathlib
+from stadtradeln_data.status import Status
+from stadtradeln_data.dataset_downloader import DownloadResult, download_dataset
+from stadtradeln_data.dataset_extractor import ExtractResult, extract_dataset
+
+
+def download(
+        year: int,
+        path: pathlib.Path,
+        overwrite: bool,
+) -> DownloadResult:
+    """
+
+    """
+    print(f"Downloading {year} dataset to path \"{path}\"")
+    retry = True
+    verify_ca_certificate = True
+    while retry:
+        download_result = download_dataset(
+            year,
+            destination_path=path,
+            verify_ca_certificate=verify_ca_certificate,
+            overwrite=overwrite
+        )
+        if download_result.status == Status.UNKNOWN_DATASET:
+            print("The dataset you requested does not exist, aborting.")
+            return download_result
+        elif download_result.status == Status.FILE_ALREADY_EXISTS:
+            print(f"File already exists, continuing.")
+            retry = False
+        elif download_result.status == Status.FAILURE:
+            print(f"Download failed, data in \"{download_result.filepath}\" seems corrupted.")
+            return download_result
+        elif download_result.status == Status.SSL_ERROR:
+            print("WARNING: Your CA-certificates could not be verified by the downloader. "
+                  "If you do not trust your network, it is advised that you download the "
+                  "files from the original BmVI website: "
+                  "https://www.mcloud.de/web/guest/suche/-/results/detail/ECF9DF02-37DC-4268-B017-A7C2CF302006 "
+                  f"and manually move them to \"{path}\"")
+            choice = input("Retry without certificate verification (Y/n)? ")
+            verify_ca_certificate = False
+            if choice != "Y" and choice != "y":
+                print("Aborting.")
+                return download_result
+        elif download_result.status == Status.SUCCESS:
+            retry = False
+        else:
+            print("Something unexpectedly went really wrong, sorry. "
+                  "Pleas submit an issue with a short description of the steps "
+                  "you made before encountering this error:")
+            print("https://github.com/Duam/stadtradeln-data/issues/new")
+            print("Aborting.")
+            return download_result
+    print(f"Dataset is located in \"{download_result.filepath}\".")
+    return download_result
+
+
+def extract(
+        year: int,
+        path: pathlib.Path,
+) -> ExtractResult:
+    """
+
+    """
+    print(f"Extracting {year} dataset in \"{path}\".")
+    extract_result = extract_dataset(year)
+    if extract_result.status == Status.UNKNOWN_DATASET:
+        print(f"The compressed dataset was not found in path \"{extract_result.filepath}\".")
+        return extract_result
+    elif extract_result.status == Status.FAILURE:
+        print("Could not extract the dataset (unknown reason).")
+        return extract_result
+    elif extract_result.status == Status.FILE_ALREADY_EXISTS:
+        print("Extracted file already exists, continuing.")
+    print(f"Raw data is located in \"{extract_result.filepath}\".")
+    return extract_result
