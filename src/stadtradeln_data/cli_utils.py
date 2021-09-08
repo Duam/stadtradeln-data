a565f5209e5d4b51e1b6b27792245acff67f39f3
Paul Daum <paul.daum@posteo.de>
2021-09-08 13:21:16 +0200

    Implemented clip() cli method.

--- src/stadtradeln_data/cli_utils.py
+++ src/stadtradeln_data/cli_utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pathlib
-from stadtradeln_data.constants import default_cache_path
+from stadtradeln_data.constants import default_cache_path, default_clipped_output_path, data_urls
 from stadtradeln_data.status import Status
 from stadtradeln_data.dataset_downloader import DownloadResult, download_dataset
 from stadtradeln_data.dataset_extractor import ExtractResult, extract_dataset
 from stadtradeln_data.dataset_clipper import ClipResult, clip_dataset
+from stadtradeln_data.pandas_importer import load_csv, write_csv
 
 
 def download(
@@ -89,15 +90,17 @@ def extract(
 def clip(
         year: int,
         path: pathlib.Path = default_cache_path,
+        output_path: pathlib.Path = None,
         overwrite: bool = False,
         latitude_min: float = -np.inf,
         latitude_max: float = np.inf,
         longitude_min: float = -np.inf,
         longitude_max: float = np.inf,
-) -> None:
+) -> ClipResult:
     """
     :param year:
     :param path:
+    :param output_path:
     :param overwrite:
     :param latitude_min:
     :param latitude_max:
@@ -105,4 +108,26 @@ def clip(
     :param longitude_max:
     :returns:
     """
-    raise NotImplementedError
+    print(f"Clipping {year} dataset in \"{path}\".")
+    print("Chosen geographical region:")
+    print(f"\t{latitude_min} <= latitude <= {latitude_max}")
+    print(f"\t{longitude_min} <= longitude <= {longitude_max}")
+    filename = pathlib.Path(data_urls[year]).with_suffix('').with_suffix('').name
+    filepath = pathlib.Path(path, filename)
+    pure_filename = pathlib.Path(filename).with_suffix('')
+
+    output_filename = pathlib.Path(f"{pure_filename}_clipped.csv")
+    output_path = pathlib.Path(output_path) if output_path is not None else pathlib.Path(path)
+    output_filepath = pathlib.Path(output_path, output_filename)
+
+    print("Loading & Clipping dataset. May take a while..")
+    df = load_csv(filepath)
+    df_clipped = clip_dataset(df, (latitude_min, latitude_max), (longitude_min, longitude_max))
+
+    print(f"Writing clipped dataset to \"{output_filepath}\".")
+    # Create output directory if it doesn't exist
+    if not output_path.is_dir():
+        output_path.mkdir(parents=True)
+
+    write_csv(df_clipped, output_filepath)
+    return ClipResult(Status.SUCCESS, "")
