dabae7522c457b563dcebc71e855bce2d7f00e59
Paul Daum <paul.daum@posteo.de>
2021-09-08 13:20:55 +0200

    Added dictionary containing the speed data links, too

--- src/stadtradeln_data/constants.py
+++ src/stadtradeln_data/constants.py
@@ -9,4 +9,26 @@ data_urls = {
           'verkehrsmengen_2020.csv.tar.gz',
 }
 
+all_data_urls = {
+    2018: {
+        'verkehrsmengen': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                          'ECF9DF02-37DC-4268-B017-A7C2CF302006/verkehrsmengen_2018.csv.tar.gz',
+        'geschwindigkeiten': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                             '33427A5A-0ADB-40B1-8A1A-390B67B0380B/geschwindigkeiten_2018.csv.tar.gz'
+    },
+    2019: {
+        'verkehrsmengen': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                          'ECF9DF02-37DC-4268-B017-A7C2CF302006/verkehrsmengen_2019.csv.tar.gz',
+        'geschwindigkeiten': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                             '33427A5A-0ADB-40B1-8A1A-390B67B0380B/geschwindigkeiten_2020.csv.tar.gz'
+    },
+    2020: {
+        'verkehrsmengen': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                          'ECF9DF02-37DC-4268-B017-A7C2CF302006/verkehrsmengen_2020.csv.tar.gz',
+        'geschwindigkeiten': 'https://www.mcloud.de/downloads/ingrid-group_ige-iplug-mcloud/'
+                             '33427A5A-0ADB-40B1-8A1A-390B67B0380B/geschwindigkeiten_2020.csv.tar.gz'
+    }
+}
+
 default_cache_path = pathlib.Path('/tmp/stadtradeln_data/')
+default_clipped_output_path = pathlib.Path(default_cache_path, "clipped/")
